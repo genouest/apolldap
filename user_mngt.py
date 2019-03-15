5c2ec4d7d8978beff0c47951723ed0990374293a
Anthony Bretaudeau <anthony.bretaudeau@inria.fr>
2019-03-15 16:38:47 +0100

    lint

--- user_mngt.py
+++ user_mngt.py
@@ -1,11 +1,13 @@
 from __future__ import print_function
 
-import sys
 import os
+import sys
+
 import ldap
+
 import requests
 
-base_apollo_url="http://0.0.0.0:8080"
+base_apollo_url = os.environ['APOLLO_URL']
 
 apollo_auth = {
     'username': os.environ['APOLLO_ADMIN'],
@@ -29,6 +31,7 @@ def apollo_get_users():
         users_mail.append(user['username'].split("@")[0])
     return users_mail
 
+
 def apollo_get_groups():
     r = requests.post(base_apollo_url + '/group/loadGroups', json=apollo_auth)
     groups = r.json()
@@ -37,6 +40,7 @@ def apollo_get_groups():
         print(group)
         group_list[group['name']] = group['id']
     return group_list
+
 
 def apollo_create_groups(groups_name_list, apollo_existing_groups):
     for group in groups_name_list:
@@ -47,6 +51,7 @@ def apollo_create_groups(groups_name_list, apollo_existing_groups):
         apollo_existing_groups[group] = res['id']
     return apollo_existing_groups
 
+
 def apollo_update_groups(groups_membership, id_table):
     for group in groups_membership:
         body = apollo_auth
@@ -56,10 +61,11 @@ def apollo_update_groups(groups_membership, id_table):
         if r.status_code != 200:
             print(r)
 
+
 def ldap_get_users(apollo_user_list):
     con = ldap.initialize(ldap_conf['url'])
     con.simple_bind_s()
-    ldap_users = con.search_s(ldap_conf['people_dn'], ldap.SCOPE_SUBTREE, '(mail=*)', ['uid','mail','cn'])
+    ldap_users = con.search_s(ldap_conf['people_dn'], ldap.SCOPE_SUBTREE, '(mail=*)', ['uid', 'mail', 'cn'])
     users = {}
     for u in ldap_users:
         # If user is not in apollo, ignore it
@@ -67,10 +73,11 @@ def ldap_get_users(apollo_user_list):
             users[u[1]['uid'][0]] = u[1]['uid'][0] + mail_suffix
     return users
 
+
 def ldap_get_groups(user_list):
     con = ldap.initialize(ldap_conf['url'])
     con.simple_bind_s()
-    ldap_groups = con.search_s(ldap_conf['group_dn'], ldap.SCOPE_SUBTREE, "cn=*", ['cn','memberUid'])
+    ldap_groups = con.search_s(ldap_conf['group_dn'], ldap.SCOPE_SUBTREE, "cn=*", ['cn', 'memberUid'])
     groups = {}
 
     for g in ldap_groups:
@@ -83,6 +90,7 @@ def ldap_get_groups(user_list):
 
     return groups
 
+
 def filter_groups(apollo_groups, ldap_groups):
     # Return list of missing groups in Apollo. What to do with missing groups in ldap? Remove from apollo?
     missing_groups = []
@@ -90,6 +98,7 @@ def filter_groups(apollo_groups, ldap_groups):
         if ldap_group not in apollo_groups:
             missing_groups.append(ldap_group)
     return missing_groups
+
 
 def is_ldap_enabled():
     if 'LDAP_ENABLED' not in os.environ:
@@ -100,6 +109,7 @@ def is_ldap_enabled():
     else:
         return False
 
+
 def main():
 
     if not is_ldap_enabled():
@@ -108,17 +118,17 @@ def main():
     apollo_user_list = apollo_get_users()
     apollo_groups = apollo_get_groups()
 
-    user_list=ldap_get_users(apollo_user_list)
-    ldap_groups=ldap_get_groups(user_list)
+    user_list = ldap_get_users(apollo_user_list)
+    ldap_groups = ldap_get_groups(user_list)
 
     missing_apollo_groups = filter_groups(apollo_groups, ldap_groups)
 
     # Create missing groups
-
     apollo_groups = apollo_create_groups(missing_apollo_groups, apollo_groups)
 
     # Populate groups
-
     apollo_update_groups(ldap_groups, apollo_groups)
 
-if __name__ == "__main__": main()
+
+if __name__ == "__main__":
+    main()
